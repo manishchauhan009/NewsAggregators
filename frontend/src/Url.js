9c895bb62b05e2f13d9ed0aea8bc419075c0de04
Chauhan Manish Umashankar <164005214+manishchauhan009@users.noreply.github.com>
2024-07-16 10:20:19 +0530

    Update Url.js

--- frontend/src/Url.js
+++ frontend/src/Url.js
@@ -1,4 +1,4 @@
-const userUrl="https://newsaggregators.onrender.com/api/user";
-const newsUrl="https://newsaggregators.onrender.com/api/news";
+const userUrl="https://newsaggregators.onrender.com:5000/api/user";
+const newsUrl="https://newsaggregators.onrender.com:5000/api/news";
 
 export default {userUrl,newsUrl};
