cb1335201d1cc0b31fa2ba363443d59d7378038a
Chauhan Manish Umashankar <164005214+manishchauhan009@users.noreply.github.com>
2024-07-18 18:20:18 +0530

    Update newsRoutes.js
    Added Middlware for authentication

--- Backend/routes/newsRoutes.js
+++ Backend/routes/newsRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { createNews, adminNews, newsData, adminApprove, adminDeny, categoryData } = require("../controllers/newsHandler");
+const { authMiddleware} = require("../middleware/authMiddleware");
 const router = express.Router();
-router.post("/createNews", createNews);
-router.get('/newsData', newsData);
+router.post("/createNews",authMiddleware, createNews);
+router.get('/newsData',authMiddleware ,newsData);
 router.get("/admin/news", adminNews);
 router.post('/admin/approve', adminApprove);
 router.post('/admin/deny', adminDeny);
-router.post('/categoryData', categoryData);
+router.post('/categoryData',authMiddleware ,categoryData);
 
 module.exports = router;
