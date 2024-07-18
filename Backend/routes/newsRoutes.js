6c70be1fbe4a2c05636f3731dfb38c33b1800535
Chauhan Manish Umashankar <164005214+manishchauhan009@users.noreply.github.com>
2024-07-18 18:45:19 +0530

    Update newsRoutes.js

--- Backend/routes/newsRoutes.js
+++ Backend/routes/newsRoutes.js
@@ -2,11 +2,23 @@ const express = require("express");
 const { createNews, adminNews, newsData, adminApprove, adminDeny, categoryData } = require("../controllers/newsHandler");
 const { authMiddleware} = require("../middleware/authMiddleware");
 const router = express.Router();
-router.post("/createNews",authMiddleware, createNews);
-router.get('/newsData',authMiddleware ,newsData);
+router.post("/createNews",createNews);
+router.get('/newsData',newsData);
 router.get("/admin/news", adminNews);
 router.post('/admin/approve', adminApprove);
 router.post('/admin/deny', adminDeny);
-router.post('/categoryData',authMiddleware ,categoryData);
+router.post('/categoryData',categoryData);
+
+
+// const express = require("express");
+// const { createNews, adminNews, newsData, adminApprove, adminDeny, categoryData } = require("../controllers/newsHandler");
+// const { authMiddleware} = require("../middleware/authMiddleware");
+// const router = express.Router();
+// router.post("/createNews",authMiddleware, createNews);
+// router.get('/newsData',authMiddleware ,newsData);
+// router.get("/admin/news", adminNews);
+// router.post('/admin/approve', adminApprove);
+// router.post('/admin/deny', adminDeny);
+// router.post('/categoryData',authMiddleware ,categoryData);
 
 module.exports = router;
