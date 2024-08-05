7b897cb4ea2dad6eb27e754508efcead0a770b5b
SkylakeFreak <utkarshbarde2@gmail.com>
2024-08-05 17:43:35 +0530

    v18

--- frontend/src/Url.js
+++ frontend/src/Url.js
@@ -1,6 +1,6 @@
-// const userUrl="https://newsaggregators.onrender.com/api/user";
-// const newsUrl="https://newsaggregators.onrender.com/api/news";
-const userUrl="http://localhost:5000/api/user";
-const newsUrl="http://localhost:5000/api/news";
+const userUrl="https://newsaggregators.onrender.com/api/user";
+const newsUrl="https://newsaggregators.onrender.com/api/news";
+// const userUrl="http://localhost:5000/api/user";
+// const newsUrl="http://localhost:5000/api/news";
 
 export default {userUrl,newsUrl};
