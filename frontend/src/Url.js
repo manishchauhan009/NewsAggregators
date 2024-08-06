8413c2ebccb0aa14adffca20e7db3a10c1d71491
Chauhan Manish Umashankar <164005214+manishchauhan009@users.noreply.github.com>
2024-08-06 11:28:23 +0530

    Update Url.js

--- frontend/src/Url.js
+++ frontend/src/Url.js
@@ -1,5 +1,7 @@
-const userUrl="https://newsaggregators.onrender.com/api/user";
-const newsUrl="https://newsaggregators.onrender.com/api/news";
+// const userUrl="https://newsaggregators.onrender.com/api/user";
+// const newsUrl="https://newsaggregators.onrender.com/api/news";
+const userUrl="https://newsaggregators-1.onrender.com/api/user";
+const newsUrl="https://newsaggregators-1.onrender.com/api/news";
 // const userUrl="http://localhost:5000/api/user";
 // const newsUrl="http://localhost:5000/api/news";
 
